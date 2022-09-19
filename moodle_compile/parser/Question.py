cd35e97a32b21cc16ea6241c3ce2a21934763d95
ctak052 <62499658+ctak052@users.noreply.github.com>
2022-09-19 14:21:11 +1200

    split Question into question types

--- moodle_compile/parser/Question.py
+++ moodle_compile/parser/Question.py
@@ -1,17 +1,19 @@
+import pathlib
+from jinja2 import Environment, FileSystemLoader
+import re
+
+
+
 class Question:
-    def __init__(self, title, prompt, grade, answer, testcases, category):
+    def __init__(self, title, prompt, grade):
         self.title = title
         self.prompt = prompt
         self.grade = grade
-        self.answer = answer
-        self.testcases = testcases
-        self.category = category
 
     def __init__(self):
         self.title = "null"
         self.prompt = "null"
-        self.answer = "null"
-        self.testcases = []
+        self.grade = 10
 
     def setTitle(self, title):
         self.title = title
@@ -22,11 +24,19 @@ class Question:
     def setGrade(self, grade):
         self.grade = grade
 
+
+class CodeRunner(Question):
+    def __init__(self):
+        self.template = 'coderunner.xml'
     def setAnswer(self, answer):
         self.answer = answer
 
-    def setTestcases(self, testcases):
+    def setCases(self, testcases):
         self.testcases = testcases
 
-    def setCategory(self, category):
-        self.category = category
+
+class MultipleChoice(Question):
+    def __init__(self):
+        self.template = 'multiplechoice.xml'
+    def setCases(self, answers):
+        self.answers = answers
